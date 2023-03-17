18d76f5cbba56fdbb62a263dd5c2e0def1efe0ee
Denock <117991030+Denock009@users.noreply.github.com>
2023-03-17 16:06:24 +0300

    Пр 17 17.03.2023

--- untitled18.py
+++ untitled18.py
@@ -6,3 +6,103 @@ while c <= 100:
     a = a + a * 0.1
     b +=  1
 print(b)
+----------------------------------------------------
+a = 10
+b = 1
+c = 0
+if c <= 100:
+    c += a
+    a = a + a * 0.1
+    b +=  1
+    if c <= 100:
+        c += a
+        a = a + a * 0.1
+        b +=  1
+        if c <= 100:
+            c += a
+            a = a + a * 0.1
+            b +=  1
+            if c <= 100:
+                c += a
+                a = a + a * 0.1
+                b +=  1
+                if c <= 100:
+                    c += a
+                    a = a + a * 0.1
+                    b +=  1
+                    if c <= 100:
+                        c += a
+                        a = a + a * 0.1
+                        b +=  1
+                        if c <= 100:
+                            c += a
+                            a = a + a * 0.1
+                            b +=  1
+                            if c <= 100:
+                                c += a
+                                a = a + a * 0.1
+                                b +=  1
+                                if c <= 100:
+                                    c += a
+                                    a = a + a * 0.1
+                                    b +=  1
+                                    if c <= 100:
+                                        c += a
+                                        a = a + a * 0.1
+                                        b +=  1
+                                        if c <= 100:
+                                            c += a
+                                            a = a + a * 0.1
+                                            b +=  1
+                                            if c <= 100:
+                                                c += a
+                                                a = a + a * 0.1
+                                                b +=  1
+                                                if c <= 100:
+                                                    c += a
+                                                    a = a + a * 0.1
+                                                    b +=  1
+                                                    if c <= 100:
+                                                        c += a
+                                                        a = a + a * 0.1
+                                                        b +=  1
+                                                        if c <= 100:
+                                                            c += a
+                                                            a = a + a * 0.1
+                                                            b +=  1
+                                                            if c <= 100:
+                                                                c += a
+                                                                a = a + a * 0.1
+                                                                b +=  1
+                                                            else:
+                                                                print(b)
+                                                        else:
+                                                            print(b)       
+                                                    else:
+                                                        print(b)
+                                                else:
+                                                    print(b)      
+                                            else:
+                                                print(b)
+                                        else:
+                                            print(b)       
+                                    else:
+                                        print(b)
+                                else:
+                                    print(b)    
+                            else:
+                                print(b)
+                        else:
+                            print(b)       
+                    else:
+                        print(b)
+                else:
+                    print(b)     
+            else:
+                print(b)
+        else:
+            print(b)            
+    else:
+        print(b)
+else:
+    print(b)       
