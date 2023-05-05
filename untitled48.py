67268d45ca8929c19efd64743e7d4fbdc20a05f9
Denock <117991030+Denock009@users.noreply.github.com>
2023-05-05 13:44:07 +0300

    Пр 1 05.05.2023

---
+++ untitled48.py
@@ -0,0 +1,14 @@
+from random import randint
+c = 0
+a = randint(0, 101)
+while True:
+    b = int(input())
+    c += 1
+    if b == a:
+        print('Вы угадали')
+        print('Количество попыток', c)
+        break
+    if b > a:
+        print('Число больше загаданного')
+    else:
+        print('Число меньше загаданного')
