b38a55a2a546cf964229de8672ae6c94b3255e54
Denock <117991030+Denock009@users.noreply.github.com>
2023-05-15 16:46:46 +0300

    Update Untitled50.py

--- Untitled50.py
+++ Untitled50.py
@@ -29,3 +29,32 @@ def schet ():
 y, u, g = schet()
 k, h, p = schet()
 t, x, v = schet()
+
+-----------------------------------------------------
+
+def schet1 ():
+    a = int(input("Введите первое число:"))
+    a = float(a)
+    b = int(input("Введите второе число:"))
+    a = float(b)
+    c = int(input("Введите третье число:"))
+    a = float(c)
+    s = int(input("Введите третье число:"))
+    s = float(c)
+    if a > b < c:
+        print (a - b + c - s)
+    else:
+        print (b + a - c + s)
+    return a, b, c, s
+a, b, c, s = schet1()
+q, w, e, r = schet1()
+def schet2 ():
+    f = a - q + b - w + c - e + s - r
+    g = a + q - b + w - c + e - s + r
+    if f == g:
+        print(f ** 2)
+    else:
+        print(a * q - s * r)
+    return a, b, c, s
+a, b, c, s = schet2()
+q, w, e, r = schet2()
